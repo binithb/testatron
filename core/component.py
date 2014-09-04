d68122dabdd0f8c8fcaebe3484b7bd1fa3a6b4c9
a-kp <anupama.kp@gmail.com>
2014-09-04 23:44:32 +0200

    1

--- core/component.py
+++ core/component.py
@@ -2,6 +2,9 @@ __author__ = 'anupama'
 
 import jinja2
 import simplejson as json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class Component:
     def __init__(self,json_file, section):
@@ -11,6 +14,8 @@ class Component:
         template = templateenv.get_template( template_file )
         self.page = json.loads( template.render(  ))
         self.span = self.page["elements"][section]
+        self.props = {}
+
         print self.span
 
     def parse(self):
@@ -18,8 +23,22 @@ class Component:
 
     def load(self):
         for element in self.span:
-            
+            print element
+            print self.span[element]
+            print self.span[element]["props"]
+            print self.span[element]["props"]["loc"]
+            self.props[element] = self._get_element(self.span[element]["props"]["loc"])
+        pass
+    def _get_element(self,loc):
+        driver = webdriver.Firefox()
+        #driver = webdriver.PhantomJS()
+        driver.get("http://www.twitter.com")
+        elem = driver.find_element_by_id("signin-email")
+        print elem.__dict__
+
         pass
 
 cmp = Component("login.json","login-span")
+cmp.load()
+
 
