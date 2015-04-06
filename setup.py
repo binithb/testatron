88bd705b79b5df349d7b7c04fe06ead644b24d94
anuapama kattiparambil prakasan <anupama@localhost.localdomain>
2015-04-06 22:58:44 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(name='testatron',
       version='0.0.1',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=['robotframework-selenium2library', 'jinja2', 'simplejson'],
       )
